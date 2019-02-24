4b3ef57e04bbbf6673aa0a105564b8dbaddf93f0
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-24 23:03:37 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const prefix = "</>";
+
+const bot = new Discord.Client();
+
+bot.on("ready", () => {
+       console.log(`${bot.user.username} is online`)
+    // bot.user.setActivity("Hello", {type: "STREAMING", url:"https://twitch.tv/Strandable"});
+
+       let statuses = [
+       //   `to ${bot.guilds.size} servers!`, //shows in how many servers I am!
+          "My prefix is </>", //my Prefix!
+          "start with </>help", //simple help!
+          "Version: 0.1-alpha!", //Bots version!
+          `around ${bot.users.size} people!` //shows how many users im managing!
+       ]
+
+    setInterval(function() {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        bot.user.setActivity(status, {type: "LISTENING"});
+        bot.user.setStatus("idle"); //online, idle, dnd
+    }, 5000)
+});
+
+
+bot.on("message", message => {
+       if(message.author.bot) return;
+       if(!message.content.startsWith(prefix)) return;
+       if(message.channel.dm) return message.author.send("Commands wont work in here.");
+       let messageArray = message.content.split(" ");
+       let cmd = messageArray[0];
+       let content = message.content;
+       let author1 = message.author.username;
+       let user = message.mentions.members.first();
+       
+       if(cmd === "help" {
+         var embed = nee Discord.RichEmbed()
+         .setAuthor(`${message.guild.name}`)
+         .setColor("GREEN");
+         message.channel.send(embed)
+       })
