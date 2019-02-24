7da13e5f5ca4b4d54e9a3512d0ab3d30ab236d7c
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-24 23:21:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,5 +39,25 @@ bot.on("message", message => {
          .setColor("GREEN");
          message.channel.send(embed)
        }
+       
+       if(cmd === "serverInfo"){
+              let emb = new Discord.RichEmbed()
+              .setColor('00ff54')
+              .setTitle('Server Info')
+              .addField(`Server Name`, message.guild.name, true)
+              .setThumbnail(message.guild.iconURL)
+              .addField(`Owner`, message.guild.owner, true)
+              .addField(`ID`, message.guild.id, true)
+              .addField(`Channels Count`, message.guild.channels.size, true)
+              .addField(`Roles Count`, message.guild.roles.size, true)
+              .addField(`Emojis Count`, message.guild.emojis.size, true)
+              .addField(`Members Count`, message.guild.members.size, true)
+              .addField(`Users Count`, message.guild.members.filter(member => !member.user.bot).size, true)
+              .addField(`Bots Count`, message.guild.members.filter(member => member.user.bot).size, true)
+              .addField(`AFK Channel`, message.guild.afkChannel, true)
+              .addField(`Server Region`, message.guild.region, true)
+              .addField(`Server Created`, message.guild.createdAt, true);
+              message.channel.send(emb);
+       }
 }); //reload
 bot.login(process.env.token)
