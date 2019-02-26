b1739b1b36220b46693baecc98c335fad52797aa
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-26 23:26:11 +0100

    Update set-prefix.js

--- commands/set-prefix.js
+++ commands/set-prefix.js
@@ -1,7 +1,7 @@
 const db = require("quick.db");
 
 module.exports.run = async (bot, message, args, tools) => {
-  if (!message.member.hasPermission('ADMINISTRATOR') return message.channel.send(":x: || Not enought permissions!")
+  if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(":x: || Not enought permissions!")
 	.then(msg => msg.delete({
 		timeout: 10000
 	}));
