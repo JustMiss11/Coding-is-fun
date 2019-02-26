1af9b7b1a6fc228a779530b0ebd04a39f84deb4b
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-26 23:25:13 +0100

    Update set-prefix.js

--- commands/set-prefix.js
+++ commands/set-prefix.js
@@ -1,11 +1,19 @@
 const db = require("quick.db");
 
 module.exports.run = async (bot, message, args, tools) => {
-  if(!message.member.hasPermission('MANAGE_SERVER')) return message.channel.send(":x: || **You dont have Manage Server permission!");
-         
-         db.set(`prefix_${message.guild.id}`, args.join(" ")).then(i => {
-                message.channel.send(`:white_check_mark: || **Sucesfully changed prefix for this guild.** ({i})`);
-         })
+  if (!message.member.hasPermission('ADMINISTRATOR') return message.channel.send(":x: || Not enought permissions!")
+	.then(msg => msg.delete({
+		timeout: 10000
+	}));
+if (!args.join(' ')) return message.channel.send(':x: || Please provide a prefix to change server prefix')
+	.then(msg => msg.delete({
+		timeout: 10000
+	}));
+
+db.set(`prefix_${message.guild.id}`, args.join(' '))
+	.then(i => {
+		message.channel.send(`Server Prefix has been changed to ${i}`);
+	})
 }
 
 module.exports.help = {
