cdd4ee10f5c06c3e3c0643ce758bd05ca057322a
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-26 23:01:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -65,26 +65,6 @@ bot.on("message", async message => {
          .setColor("GREEN");
          message.channel.send(embed)
        }
-       //SERVER INFO CMD
-       if(cmd === `${prefix}serverInfo`){
-              let emb = new Discord.RichEmbed()
-              .setColor('00ff54')
-              .setTitle('Server Info')
-              .addField(`Server Name`, message.guild.name, true)
-              .setThumbnail(message.guild.iconURL)
-              .addField(`Owner`, message.guild.owner, true)
-              .addField(`ID`, message.guild.id, true)
-              .addField(`Channels Count`, message.guild.channels.size, true)
-              .addField(`Roles Count`, message.guild.roles.size, true)
-              .addField(`Emojis Count`, message.guild.emojis.size, true)
-              .addField(`Members Count`, message.guild.members.size, true)
-              .addField(`Users Count`, message.guild.members.filter(member => !member.user.bot).size, true)
-              .addField(`Bots Count`, message.guild.members.filter(member => member.user.bot).size, true)
-              .addField(`AFK Channel`, message.guild.afkChannel, true)
-              .addField(`Server Region`, message.guild.region, true)
-              .addField(`Server Created`, message.guild.createdAt, true);
-              message.channel.send(emb);
-       }
        //SERVER ICON CMD
        if(cmd === `${prefix}serverIcon`){
               var embed = new Discord.RichEmbed()
