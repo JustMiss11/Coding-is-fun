e2954ffa58b0630131f8912d5967d3158a0e9901
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-27 22:32:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -60,9 +60,41 @@ bot.on("message", async message => {
        if(!message.content.startsWith(prefix)) return;
        if(message.channel.dm) return message.author.send("Commands wont work in here.");
        let messageArray = message.content.split(" ");
-    
+       if(message.content === `{prefix}help`) {
+         var embed = new Discord.RichEmbed()
+         .setAuthor(`${message.guild.name}`)
+         .addField("serverInfo", "Shows info about server")
+         .addField("serverIcon", "Shows icon of the server")
+         .setColor("GREEN");
+         message.channel.send(embed)
+       };
        
-        
+  if(message.content === `{prefix}server-info`) {
+     let emb = new Discord.RichEmbed()
+              .setColor('00ff54')
+              .setTitle('Server Info')
+              .addField(`Server Name`, message.guild.name, true)
+              .setThumbnail(message.guild.iconURL)
+              .addField(`Owner`, message.guild.owner, true)
+              .addField(`ID`, message.guild.id, true)
+              .addField(`Channels Count`, message.guild.channels.size, true)
+              .addField(`Roles Count`, message.guild.roles.size, true)
+              .addField(`Emojis Count`, message.guild.emojis.size, true)
+              .addField(`Members Count`, message.guild.members.size, true)
+              .addField(`Users Count`, message.guild.members.filter(member => !member.user.bot).size, true)
+              .addField(`Bots Count`, message.guild.members.filter(member => member.user.bot).size, true)
+              .addField(`AFK Channel`, message.guild.afkChannel, true)
+              .addField(`Server Region`, message.guild.region, true)
+              .addField(`Server Created`, message.guild.createdAt, true);
+              message.channel.send(emb);
+  };
+  if(message.content === `{prefix}server-icon`){
+      var embed = new Discord.RichEmbed()
+              .setAuthor(`${message.guild.name}`)
+              .setImage(message.guild.iconURL)
+              .setColor("BLUE");
+              message.channel.send(embed)
+  };
             
 }); //reload
 bot.login(process.env.token)
